cf7d0188862678adaf91d3eb7b0102cfba794196
Ricards Taujenis <MozesThegreat@yahoo.com>
2022-10-12 22:12:46 +0200

    refactored results.vue

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,9 +1,11 @@
 <template>
   <v-container class="py-6 py-lg-12 text-center">
     <h1 class="text-h4 text-sm-h3 text-md-h2 text-lg-h1">Congratiulations your score is as follows</h1>
-    <h2 class="text-h6 text-sm-h5 secondary--text mt-4 w-full w-md-8-12 w-xl-half mx-auto">Overall {{this.results[1]}}/{{this.results[0]}} correct. In percentage you got {{this.percent}}% correct.</h2>
+    <h2 class="text-h6 text-sm-h5 secondary--text mt-4 w-full w-md-8-12 w-xl-half mx-auto">
+      Overall {{this.results[1]}}/{{this.results[0]}} correct. In percentage you got {{this.percent}}% correct.</h2>
     <div class="mt-4">
-      <v-btn x-large class="my-1 mx-sm-1 w-full w-sm-auto" color="primary" @click="newGame">Play Again</v-btn>
+      <v-btn x-large class="my-1 mx-sm-1 w-full w-sm-auto" color="primary" 
+      @click="newGame">Play Again</v-btn>
     </div>
   </v-container>
 </template>
